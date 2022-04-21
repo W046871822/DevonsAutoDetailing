9dbacb6216a4c2d5a23d58ac2953a5d44fcbeb49
MadisonBoone109 <87313571+W046871822@users.noreply.github.com>
2022-04-21 19:41:21 -0300

    .

---
+++ JS/ADJS.js
@@ -0,0 +1,16 @@
+//responsive nav
+//Menu toggle-button for small screens
+$(document).ready(function (){
+    $(".menu-icon").on("click", function() {
+      $("nav ul").toggleClass("showing");
+    });
+  });
+  
+  //Scrolling Effect for nav
+  $(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+      $('nav').addClass('black');
+    } else {
+      $('nav').removeClass('black');
+    }
+  });
